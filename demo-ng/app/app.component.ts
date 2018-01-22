69e1743635de663d7d769eb714114c916239ad47
Brad Martin <bradwaynemartin@gmail.com>
2018-01-22 16:39:22 -0600

    update ng import

--- demo-ng/app/app.component.ts
+++ demo-ng/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
-import { CameraPlus } from 'nativescript-camera-plus';
-import { registerElement } from 'nativescript-angular/element-registry';
-registerElement('CameraPlus', () => <any>CameraPlus);
+import { CameraPlus } from "@nstudio/nativescript-camera-plus";
+import { registerElement } from "nativescript-angular/element-registry";
+registerElement("CameraPlus", () => <any>CameraPlus);
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html",
+  selector: "ns-app",
+  templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent {}
