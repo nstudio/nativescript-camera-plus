4141909ea60d7bb1e90b4827c82310d43825c75c
Alex Miller <codex.nz@gmail.com>
2020-12-27 16:58:51 +1300

    demo: Redo main-page for better logging.

--- demo/app/main/main-page.ts
+++ demo/app/main/main-page.ts
@@ -1,4 +1,5 @@
 import { EventData, Page } from '@nativescript/core';
+import { CameraPlus } from '@nstudio/nativescript-camera-plus/camera-plus.ios';
 import { HelloWorldModel } from './main-view-model';
 
 // Event handler for Page 'loaded' event attached in main-page.xml
@@ -9,12 +10,46 @@ export function pageLoaded(args: EventData) {
 }
 
 export function camLoaded(args: any) {
-  console.info('cam loaded event');
+  const cam = args.object as CameraPlus;
+  console.info('Cam loaded event');
 
+  (async () => {
+    if (!cam.hasCameraPermission()) {
+      await cam.requestCameraPermissions();
+    }
+    cam.autoFocus = true;
+
+    logFlashMode(cam);
+    logAvailablePictureSizes(cam);
+    logSupportRatios(cam);
+  })();
+}
+
+function logFlashMode(cam: CameraPlus): void {
   try {
-    const flashMode = args.object.getFlashMode();
-    console.info(`flashMode in loaded event = ${flashMode}`);
-  } catch (e) {
-    console.error(e);
+    const flashMode = cam.getFlashMode();
+    console.info(`Flash Mode: ${flashMode}`);
+  } catch (error) {
+    console.error(`Flash Mode: Failed to load: ${error.message}`);
+    console.error(error.stack);
+  }
+}
+
+function logAvailablePictureSizes(cam: CameraPlus): void {
+  const ratio = '16:9';
+  const availableSizes = cam.getAvailablePictureSizes('16:9');
+  console.info(`Picture Sizes Available for ${ratio}:`);
+  for (const size of availableSizes) {
+    console.info(`Height: ${size.height}, Width: ${size.width}`);
+  }
+  console.info(`Total sizes available: ${availableSizes.length}`);
+}
+
+function logSupportRatios(cam: CameraPlus): void {
+  const supportedRatios = cam.getSupportedRatios();
+  if (supportedRatios.length === 0) {
+    console.warn('Ratios supported: None found.')
+  } else {
+    console.info('Ratios supported:' + supportedRatios.join(', '));
   }
 }
